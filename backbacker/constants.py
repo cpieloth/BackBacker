3c865096f09b2e254e651e6168105f4b7ca0b701
Christof Pieloth <cpieloth@gmx.net>
2014-07-07 23:20:36 +0200

    [DOC] Group parameter.

--- backbacker/constants.py
+++ backbacker/constants.py
@@ -2,21 +2,26 @@ __author__ = 'Christof Pieloth'
 
 
 class Parameter(object):
+    """Parameter for a consistent use in commands, tasks and job file."""
+    # Paths and files
     SRC_DIR = 'src_dir'
     DEST_DIR = 'dest_dir'
     DIR = 'dir'
-
-    URL = 'url'
-
     CONFIG_FILE = 'cfg_file'
 
-    DATE_FORMAT = 'datefmt'
+    # Network
+    URL = 'url'
 
-    ROTATE = 'rotate'
-
-    DB_NAME = 'db_name'
+    # Authentication
     USER = 'user'
     PASSWD = 'passwd'
+
+    # Database
+    DB_NAME = 'db_name'
+
+    # Misc
+    DATE_FORMAT = 'datefmt'
+    ROTATE = 'rotate'
 
 
 class Constants(object):
