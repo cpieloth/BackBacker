0d521432bcbbea6da071ed454e9b7605ec6c37f5
Christof Pieloth <cpieloth@gmx.net>
2019-08-06 22:05:59 +0200

    [ADD #011] Add ABC for file sync implementations.

--- backbacker/commands/file_sync.py
+++ backbacker/commands/file_sync.py
@@ -1,6 +1,8 @@
+import abc
 import logging
 import os
 import subprocess
+from abc import ABCMeta
 
 from backbacker.command import SystemCommand, CliCommand, Argument
 
@@ -9,25 +11,16 @@ __author__ = 'Christof Pieloth'
 log = logging.getLogger(__name__)
 
 
-class Rsync(SystemCommand):
+class FileSync(SystemCommand, metaclass=abc.ABCMeta):
     """
-    Wrapper for rsync.
-    rsync is a file transfer program capable of efficient remote update via a fast differencing algorithm.
+    Abstract base class for file sync commands, which provides an interface on API level.
     """
 
-    ALL = '-a'  # -r -l -p -t -g -o -D
-    DELETE_DEST = '--delete'
-    BACKUP_DELETE = '-b'
-    BACKUP_DELETE_DIR = '--backup-dir='
-    REMOTE_SHELL = '-e'
-
-    def __init__(self):
-        super().__init__('rsync')
+    def __init__(self, cmd):
+        super().__init__(cmd)
         self._src_dir = ''
         self._dst_dir = ''
-        self._backup_dir = ''
         self.mirror = False
-        self.rsh = ''
 
     @property
     def src_dir(self):
@@ -45,6 +38,34 @@ class Rsync(SystemCommand):
     def dst_dir(self, value):
         self._dst_dir = os.path.expanduser(value)
 
+
+class FileSyncCliCommand(CliCommand, metaclass=ABCMeta):
+    """Abstract base class for file sync commands, which provides an interface on CLI level."""
+
+    @classmethod
+    def _add_arguments(cls, parser):
+        parser.add_argument(Argument.SRC_DIR.long_arg, help='Source directory for sync.', required=True)
+        parser.add_argument(Argument.DST_DIR.long_arg, help='Destination directory for sync.', required=True)
+        parser.add_argument(Argument.MIRROR.long_arg, help='Enable mirror mode.', action='store_true')
+
+
+class Rsync(FileSync):
+    """
+    Wrapper for rsync.
+    rsync is a file transfer program capable of efficient remote update via a fast differencing algorithm.
+    """
+
+    ALL = '-a'  # -r -l -p -t -g -o -D
+    DELETE_DEST = '--delete'
+    BACKUP_DELETE = '-b'
+    BACKUP_DELETE_DIR = '--backup-dir='
+    REMOTE_SHELL = '-e'
+
+    def __init__(self):
+        super().__init__('rsync')
+        self._backup_dir = ''
+        self.rsh = ''
+
     @property
     def backup_dir(self):
         return self._backup_dir
@@ -60,28 +81,26 @@ class Rsync(SystemCommand):
             if not os.access(self.dst_dir, os.W_OK):
                 raise PermissionError('No write access to: {}'.format(self.dst_dir))
 
-        cmd = [self.cmd, Rsync.ALL]
+        cmd = [self.cmd, self.ALL]
         if self.mirror:
-            cmd.append(Rsync.DELETE_DEST)
+            cmd.append(self.DELETE_DEST)
             if self.backup_dir != '':
-                cmd.append(Rsync.BACKUP_DELETE)
-                cmd.append(Rsync.BACKUP_DELETE_DIR + self.backup_dir)
+                cmd.append(self.BACKUP_DELETE)
+                cmd.append(self.BACKUP_DELETE_DIR + self.backup_dir)
         if self.rsh != '':
-            cmd.append(Rsync.REMOTE_SHELL + ' ' + self.rsh)
+            cmd.append(self.REMOTE_SHELL + ' ' + self.rsh)
         cmd.append(self.src_dir)
         cmd.append(self.dst_dir)
 
         subprocess.check_call(cmd)
 
 
-class RsyncCliCommand(CliCommand):
+class RsyncCliCommand(FileSyncCliCommand):
 
     @classmethod
     def _add_arguments(cls, parser):
-        parser.add_argument(Argument.SRC_DIR.long_arg, help='Source directory for sync.', required=True)
-        parser.add_argument(Argument.DST_DIR.long_arg, help='Destination directory for sync.', required=True)
+        FileSyncCliCommand._add_arguments(parser)
         parser.add_argument(Argument.BACKUP_DIR.long_arg, help='Backup directory for rsync.')
-        parser.add_argument(Argument.MIRROR.long_arg, help='Enable mirror mode.', action='store_true')
         parser.add_argument(Argument.SHELL.long_arg, help='Remote shell to use.')
 
     @classmethod
@@ -94,7 +113,7 @@ class RsyncCliCommand(CliCommand):
 
     @classmethod
     def _instance(cls, args):
-        instance = Rsync()
+        instance = cls()
         instance.src_dir = Argument.SRC_DIR.get_value(args)
         instance.dst_dir = Argument.DST_DIR.get_value(args)
         instance.mirror = Argument.MIRROR.get_value(args)
