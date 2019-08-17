7cf29ce1a49040ff3b0a46f4b4cfa4b35093c6bf
Christof Pieloth <cpieloth@gmx.net>
2019-08-17 21:15:35 +0200

    [ADD #011] Add more unit tests robocopy.

--- tests/backbacker/commands/file_sync/test_robocopy.py
+++ tests/backbacker/commands/file_sync/test_robocopy.py
@@ -23,6 +23,10 @@ class RobocopyTestCase(unittest.TestCase):
         self._test_dir_tmp.cleanup()
 
     def test_mirror_initial(self):
+        """
+        Sync src dir into an empty dst dir.
+        All files from src dir must be part of dst dir.
+        """
         fs = file_sync.Robocopy()
         fs.src_dir = self._src_dir
         fs.dst_dir = self._dst_dir
@@ -31,3 +35,39 @@ class RobocopyTestCase(unittest.TestCase):
 
         self.assertTrue(os.path.isfile(os.path.join(self._dst_dir, 'foo', 'foo.txt')))
         self.assertTrue(os.path.isfile(os.path.join(self._dst_dir, 'bar.txt')))
+
+    def test_mirror_identical(self):
+        """
+        Sync src dir into identical dst dir.
+        All files from src dir must be part of dst dir.
+        In Addition: no files should be transferred, but not tested.
+        """
+        shutil.rmtree(self._dst_dir)
+        shutil.copytree(self._fixtures_dir, self._dst_dir)
+
+        fs = file_sync.Robocopy()
+        fs.src_dir = self._src_dir
+        fs.dst_dir = self._dst_dir
+        fs.mirror = True
+        fs.execute()
+
+        self.assertTrue(os.path.isfile(os.path.join(self._dst_dir, 'foo', 'foo.txt')))
+        self.assertTrue(os.path.isfile(os.path.join(self._dst_dir, 'bar.txt')))
+
+    def test_mirror_removed(self):
+        """
+        src dir and dst dir are identical, but bar.txt is removed in src dir.
+        After sync it must be removed in dst dir, too.
+        """
+        shutil.rmtree(self._dst_dir)
+        shutil.copytree(self._fixtures_dir, self._dst_dir)
+        os.remove(os.path.join(self._src_dir, 'bar.txt'))
+
+        fs = file_sync.Robocopy()
+        fs.src_dir = self._src_dir
+        fs.dst_dir = self._dst_dir
+        fs.mirror = True
+        fs.execute()
+
+        self.assertTrue(os.path.isfile(os.path.join(self._dst_dir, 'foo', 'foo.txt')))
+        self.assertFalse(os.path.isfile(os.path.join(self._dst_dir, 'bar.txt')))
